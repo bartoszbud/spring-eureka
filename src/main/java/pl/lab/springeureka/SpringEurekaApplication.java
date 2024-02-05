122c5acec0e06129a3af4468c4aa014365923c8d
bartek <bartoszbud@gmail.com>
2024-02-05 19:28:59 +0100

    initial commit

---
+++ src/main/java/pl/lab/springeureka/SpringEurekaApplication.java
@@ -0,0 +1,13 @@
+package pl.lab.springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaApplication.class, args);
+	}
+
+}
