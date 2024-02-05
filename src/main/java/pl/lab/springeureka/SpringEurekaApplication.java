d6be39bd688f12b4751092d6e96293a409f91e9c
bartek <bartoszbud@gmail.com>
2024-02-05 19:36:09 +0100

    properties tuning

--- src/main/java/pl/lab/springeureka/SpringEurekaApplication.java
+++ src/main/java/pl/lab/springeureka/SpringEurekaApplication.java
@@ -2,8 +2,10 @@ package pl.lab.springeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
+@EnableEurekaServer
+
 public class SpringEurekaApplication {
 
 	public static void main(String[] args) {
